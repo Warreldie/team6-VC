35c0f3699e1e684879c77645f24df8ec3005d836
Medina Dadurgova <medina-dadurgova@hotmail.com>
2021-05-14 11:04:51 +0200

    feature: signup

---
+++ controllers/api/auth.js
@@ -0,0 +1,22 @@
+const User = require('../../models/User');
+const passport = require('../../passport/passport');
+
+const signup = async (req, res, next) => {
+    let username = req.body.username;
+    let email = req.body.username;
+    let password = req.body.username;
+    
+    const user = new User({username: username});
+    await user.setPassword(password);
+    await user.save().then(result => {
+        res.json({
+            'status': 'success'
+        })
+    }).catch(error => {
+        res.json({
+            'status': 'error'
+        })
+        })
+};
+
+module.exports.signup = signup;
