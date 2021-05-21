47a1f1fd466873975c33e1742c8bbed8862d03b5
Medina Dadurgova <medina-dadurgova@hotmail.com>
2021-05-21 17:12:01 +0200

    fix: moved transfer route to separate file

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var transfersRouter = require('./routes/transfers');
+
 
 const mongoose = require('mongoose');
 mongoose.set('useCreateIndex', true);
@@ -25,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/transfers', transfersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/transfers.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+const transfersController = require('../controllers/transfers');
+
+// register
+router.get('/', function(req, res, next) {
+  res.render('register', { title: 'coins. | Register' });
+});
+
+router.post('/create', transfersController.create);
+
+module.exports = router;
