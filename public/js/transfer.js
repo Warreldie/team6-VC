7d2bbbdbbb9bd4bd46604341b818c5c04cae4dcc
Medina Dadurgova <medina-dadurgova@hotmail.com>
2021-05-30 13:57:11 +0200

    feat: initialize primus

--- public/js/transfer.js
+++ public/js/transfer.js
@@ -16,6 +16,8 @@ function searchUser(searchUser) {
             console.log(user['user']);
             let userList = `<div class="dialog__form-list"><ul><li>${user['user']}</li></ul>`;
 
+            
+
             userSearch.insertAdjacentHTML('afterend', userList);
         })
     }).catch(err => {
